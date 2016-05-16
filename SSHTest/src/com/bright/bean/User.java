dd710d31968a6f1647efc87b93d86c661243bbcb
brightvan <981947675@qq.com>
2016-05-16 21:15:46 +0800

    init commit

---
+++ SSHTest/src/com/bright/bean/User.java
@@ -0,0 +1,19 @@
+package com.bright.bean;
+
+/**
+ * User entity. @author MyEclipse Persistence Tools
+ */
+public class User extends AbstractUser implements java.io.Serializable {
+
+	// Constructors
+
+	/** default constructor */
+	public User() {
+	}
+
+	/** full constructor */
+	public User(String username, String password, String nickname) {
+		super(username, password, nickname);
+	}
+
+}
